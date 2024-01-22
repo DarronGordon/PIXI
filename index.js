c81a19b09d7e164af7068d88e5c55375b093616a
Darron <darronmgordon@gmail.com>
2024-01-22 21:01:39 +0200

    Update

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
+//import { Vertices } from "matter-js";
 import * as PIXI from "pixi.js";
-//import Victor from "victor";
+import Victor from "victor";
+import Player from "./player";
+import Zombie from "./zombie";
 //import Matter from "matter-js";
 
 let canvasSize = 256;
 const canvas = document.getElementById("mycanvas");
 const app = new PIXI.Application({
-  view: canvas,
-  width: canvasSize,
-  height: canvasSize,
-  backgroundColor: 0x5c812f,
+view: canvas,
+width: canvasSize,
+height: canvasSize,
+backgroundColor: 0x5c812f,
 });
 
-let squareWidth = 32;
-const square = new PIXI.Sprite(PIXI.Texture.WHITE);
-square.anchor.set(0.5);
-square.position.set(app.screen.width / 2, app.screen.height / 2);
-square.width = square.height = squareWidth;
-square.tint = 0xea985d;
+let player = new Player({app});
+let zombie = new Zombie({app, player});
 
-app.stage.addChild(square);
+app.ticker.add((delta) =>{
+
+player.update();
+zombie.update();
+
+});
---
+++ zombie.js
@@ -0,0 +1,70 @@
+
+import * as PIXI from "pixi.js";
+import Victor from "victor";
+
+export default class Zombie{
+    constructor({app, player})
+    {
+        this.app = app;
+        this.player = player;
+
+        let enemyRadius =16;
+        this.speed = 2;
+        
+        this.zombie = new PIXI.Graphics();
+        let r = this.RandomSpawnPoint();
+        this.zombie.position.set(r.x,r.y);
+        this.zombie.beginFill(0xFF0000, 1);
+        this.zombie.drawCircle(0,0,enemyRadius);
+        this.zombie.endFill();
+        
+        app.stage.addChild(this.zombie);
+
+
+
+    }
+
+    update()
+    {
+        let e = new Victor(this.zombie.position.x, this.zombie.position.y);
+        let s = new Victor(this.player.position.x, this.player.position.y);
+
+        if(e.distance(s) < this.player.width / 2)
+        {
+        let r = this.RandomSpawnPoint();
+        this.zombie.position.set(r.x,r.y);
+        return;
+        }
+
+        let d = s.subtract(e);
+        let v = d.normalize().multiplyScalar(this.speed);
+        this.zombie.position.set(this.zombie.position.x + v.x, this.zombie.position.y + v.y);
+    }
+
+    RandomSpawnPoint()
+    {
+        let edge = Math.floor(Math.random() * 4);
+        let SpawnPoint = new Victor(0,0);
+        let canvasSize = this.app.screen.width;
+
+        switch(edge){
+        case 0:
+        SpawnPoint.x = canvasSize * Math.random();
+        break;
+        case 1:
+            SpawnPoint.x = canvasSize;
+            SpawnPoint.y = canvasSize * Math.random();
+        break;
+        case 2:
+            SpawnPoint.x = canvasSize * Math.random();
+            SpawnPoint.y = canvasSize;
+        break;
+        default:
+            SpawnPoint.x =0;
+            SpawnPoint.y = canvasSize * Math.random();
+        break;
+        }
+
+        return SpawnPoint;
+    }
+}
